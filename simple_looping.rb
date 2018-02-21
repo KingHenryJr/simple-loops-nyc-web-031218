ae3069a46f9504841b3ad52211159f1ef9174fc2
Henry Zhang <henryyihengzhang@gmail.com>
2018-02-21 18:56:10 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -51,7 +51,9 @@ def for_iterator(number_of_times)
   
   phrase = "Welcome to Flatiron School's Web Development Course!"
   
-  counter =
+  counter = 1..number_of_times
+  
+  for blah 
   
 end
 
